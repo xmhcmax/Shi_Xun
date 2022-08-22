ccbd7c88974b4c4cf8dfa4006751179afb696b88
xmhcmax <102151492+xmhcmax@users.noreply.github.com>
2022-08-22 13:59:26 +0800

    Delete test directory

--- test/cout_test.cpp
+++
@@ -1,9 +0,0 @@
-#include"D:\apps\VS code\Programs\C++ program\shixun\data_in_disk\data_in_disk.h"
-using namespace std;
-//测试cout
-
-// int main(){
-//     int i =1;
-//     cout<<"a is "<<i<<" missing"<<endl;
-//     return 0;
-// }
