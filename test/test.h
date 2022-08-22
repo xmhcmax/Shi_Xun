bb6c60f15f5e3bf953703a5811038295e159522b
xmhcmax <102151492+xmhcmax@users.noreply.github.com>
2022-08-23 00:48:50 +0800

    Delete test directory

--- test/test.h
+++
@@ -1,5 +0,0 @@
-#include"D:\apps\VS code\Programs\C++ program\shixun\data_in_disk\data_in_disk.h"
-// #include"D:\apps\VS code\Programs\C++ program\shixun\data_in_mem_forN\data_in_mem_forN.h"
-using namespace std;
-
-extern char cs[10];
