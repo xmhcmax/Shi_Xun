e04aa224a8a5edde52931f8bac819be1dbc79ad1
xmhcmax <102151492+xmhcmax@users.noreply.github.com>
2022-08-31 12:41:01 +0800

    Delete test directory

--- test/print_char_test.cpp
+++
@@ -1,8 +0,0 @@
-#include"..\\main\\data_in_disk_forN\\data_in_disk_forN.h"
-using namespace std;
-
-int main(){
-    char c='1';
-    cout<<c<<endl;  
-    return 0;
-}
--- test/string_to_number_test.cpp
+++
@@ -1,9 +0,0 @@
-#include"..\\main\\data_in_disk_forN\\data_in_disk_forN.h"
-using namespace std;
-
-int main(){
-    string str = "1";
-    int number=stoi(str,0,10);
-    cout<<number<<endl;
-    return 0;
-}
